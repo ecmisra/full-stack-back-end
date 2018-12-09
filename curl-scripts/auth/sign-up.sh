2481aa6c041373f8e32a95494b605b37a7436fed
ecmisra <eshanmisra@gmail.com>
2018-12-09 16:00:31 -0500

    update director working

--- curl-scripts/auth/sign-up.sh
+++ curl-scripts/auth/sign-up.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# BAhJIiVkYjkzYjhhYjY0ZDFhM2U5Y2IzMDU1MDI0NmYyMjNhZgY6BkVG--121d44b1d97d92087ddac16d3233e4116a30750c
 
 curl "http://localhost:4741/sign-up" \
   --include \
--- curl-scripts/directors/update-director.sh
+++ curl-scripts/directors/update-director.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl "http://localhost:4741/directors/${ID}" \
+  curl "http://localhost:4741/directors/${ID}" \
   --include \
   --request PATCH \
   --header "Content-Type: application/json" \
